30ebd237e13af5c18a39415215458557119d6a30
KOMPTUK-10 <luthfi.fahrizal11@gmail.com>
2022-06-04 09:54:18 +0700

    Second Commit

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   });
 })
 
+app.post('/', function (req, res) {
+  res.json({
+    type: 'post',
+    number: 1,
+    nama: 'Luthfi Fahrizal Al Ghifari',
+    npm: '2042401'
+  });
+})
+
 app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
